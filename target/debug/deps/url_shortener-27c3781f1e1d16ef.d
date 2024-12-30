9f0cdf889bf7f123f29a90b7222ec0c6e70c7a7a
Yuko Pangestu <yuko.pangestu@paper.id>
2024-12-30 16:37:05 +0700

    cargo init

---
+++ target/debug/deps/url_shortener-27c3781f1e1d16ef.d
@@ -0,0 +1,5 @@
+G:\Rust\url-shortener\target\debug\deps\liburl_shortener-27c3781f1e1d16ef.rmeta: src\main.rs
+
+G:\Rust\url-shortener\target\debug\deps\url_shortener-27c3781f1e1d16ef.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/url_shortener-e2984121e80684a4.d
@@ -0,0 +1,5 @@
+G:\Rust\url-shortener\target\debug\deps\liburl_shortener-e2984121e80684a4.rmeta: src\main.rs
+
+G:\Rust\url-shortener\target\debug\deps\url_shortener-e2984121e80684a4.d: src\main.rs
+
+src\main.rs:
